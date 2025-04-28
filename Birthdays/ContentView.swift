608b18491b399113c82f208f57cb22d72fccbfcf
tong-dai <td2471@princeton.edu>
2025-04-27 20:12:49 -0400

    project done

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    static let formatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy/MM/dd"
+            return formatter
+    }()
+    
+    @State private var friends: [Friend] = [
+        Friend(name: "Carolyn Song", birthday: formatter.date(from:"2003/05/04") ?? Date.now),
+        Friend(name: "Bonnie Dai", birthday: formatter.date(from:"1999/11/15") ?? Date.now)
+    ]
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month().day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center) {
+                    Text("New Birthday")
+                        .fontWeight(.bold)
+                        .padding(10)
+                        
+                    DatePicker(selection: $newBirthday,
+                        in: Date.distantPast...Date.now,
+                        displayedComponents: .date
+                    ) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(PlainTextFieldStyle())
+                    }
+                    .padding(.horizontal, 40)
+                    
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .fontWeight(.bold)
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
---
+++ Birthdays/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Tong Dai on 4/27/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
